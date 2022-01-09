ef27b2a5447c9cc020f80cc96e88830d02cfc033
Huac233 <96987145+Huac233@users.noreply.github.com>
2022-01-09 19:03:24 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -85,7 +85,7 @@ check_region() {
 Download_Server_Status_server() {
   cd "/tmp" || exit 1
   [[ ${mirror_num} == 2 ]] && bundle_link="https://github.com.cnpmjs.org/CokeMine/ServerStatus-Hotaru/archive/master.zip" || bundle_link="https://ghproxy.com/https://github.com/CokeMine/ServerStatus-Hotaru/archive/master.zip"
-  [[ ${mirror_num} == 2 ]] && github_link="https://github.com.cnpmjs.org/" || github_link="https://github.com"
+  [[ ${mirror_num} == 2 ]] && github_link="https://github.com.cnpmjs.org/" || github_link="https://ghproxy.com/https://github.com"
   wget -N --no-check-certificate "${bundle_link}" -O "master.zip"
   [[ ! -e "master.zip" ]] && echo -e "${Error} ServerStatus 服务端下载失败 !" && exit 1
   unzip master.zip
